3e4e3b0e2dfa499b05b854c9c30cfca70bf4fe8d
kabzon93region <65691103+kabzon93region@users.noreply.github.com>
2021-12-24 02:24:11 +0300

    1
    1

--- Deep-Projection/mainForm.cpp
+++ Deep-Projection/mainForm.cpp
@@ -1,2 +1,18 @@
 #include "mainForm.h"
 
+// Required namespaces
+using namespace System;
+using namespace System::Windows::Forms;
+
+// Application entry point
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	// Initial Application Parameters:
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	// ChartCPP - name of your project / namespace of your project
+	// MyFormMyForm - name of the form to launch
+	DeepProjection::mainForm form;
+	Application::Run(% form);
+}
--- Deep-Projection/mainForm.h
+++ Deep-Projection/mainForm.h
@@ -48,12 +48,18 @@ namespace DeepProjection {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"mainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// mainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(484, 275);
+			this->Name = L"mainForm";
+			this->Text = L"mainForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
-	};
+};
 }
